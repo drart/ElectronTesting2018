0ef295b5e9b390ff605db1a69e244bdbb7a8586e
Adam Tindale <adam@adamtindale.com>
2018-05-22 19:02:56 -0400

    basic osc.js demo working

--- main.js
+++ main.js
@@ -16,9 +16,8 @@ const {app, BrowserWindow} = require('electron')
       protocol: 'file:',
       slashes: true
     }))
-  
-    // Open the DevTools.
-    win.webContents.openDevTools()
+
+ win.webContents.openDevTools()  
   
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -53,3 +52,60 @@ const {app, BrowserWindow} = require('electron')
   
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
+var osc = require("osc"),
+    WebSocket = require("ws")
+
+
+var getIPAddresses = function () {
+    var os = require("os"),
+    interfaces = os.networkInterfaces(),
+    ipAddresses = [];
+
+    for (var deviceName in interfaces){
+        var addresses = interfaces[deviceName];
+
+        for (var i = 0; i < addresses.length; i++) {
+            var addressInfo = addresses[i];
+
+            if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+                ipAddresses.push(addressInfo.address);
+            }
+        }
+    }
+
+    return ipAddresses;
+};
+
+var udp = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 7400,
+    remoteAddress: "127.0.0.1",
+    remotePort: 7500
+});
+
+udp.on("ready", function () {
+    var ipAddresses = getIPAddresses();
+    console.log("Listening for OSC over UDP.");
+    ipAddresses.forEach(function (address) {
+        console.log(" Host:", address + ", Port:", udp.options.localPort);
+    });
+    console.log("Broadcasting OSC over UDP to", udp.options.remoteAddress + ", Port:", udp.options.remotePort);
+});
+
+udp.open();
+
+var wss = new WebSocket.Server({
+    port: 8081
+});
+
+wss.on("connection", function (socket) {
+    console.log("A Web Socket connection has been established!");
+    var socketPort = new osc.WebSocketPort({
+        socket: socket
+    });
+
+    var relay = new osc.Relay(udp, socketPort, {
+        raw: true
+    });
+});
+
